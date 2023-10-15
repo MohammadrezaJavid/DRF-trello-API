ca4bc40a1bd88cccb62e9b76db4e802576c1c143
mohammadrezajavid <javid.9@yahoo.com>
2023-10-15 14:37:53 +0330

    update urls for list objects

--- trello/urls.py
+++ trello/urls.py
@@ -5,8 +5,18 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'boards', viewset=views.BoardView, basename='board')
 
+list_list = views.ListView.as_view({
+    'post': 'create'
+})
+list_detail = views.ListView.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
 urlpatterns = [
-    # path('board/', views.BoardView.as_view({'get': 'list'}), name='board-list'),
-    # path('board/<int:id>/', views.BoardView.as_view({'get': 'detail'}), name='board-detail'),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('lists/', list_list, name='list-list'),
+    path('lists/<int:id>/', list_detail, name='list-detail'),
 ]
