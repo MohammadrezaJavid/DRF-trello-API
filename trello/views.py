342592fbfc91e288ec33688556bfb77b36c5f7df
mohammadrezajavid <javid.9@yahoo.com>
2023-10-15 14:38:31 +0330

    add ListView

--- trello/views.py
+++ trello/views.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets
 from . import models, serializers
-from .permissions import IsOwnerOnly
+from .permissions import IsOwnerOnlyOrPublic, IsAccessToList
 
 
 class BoardView(viewsets.ModelViewSet):
     lookup_field = 'id'
-    permission_classes = (IsOwnerOnly,)
+    permission_classes = (IsOwnerOnlyOrPublic,)
     queryset = models.Board.objects.all()
     serializer_class = serializers.BoardSerializer
 
@@ -13,9 +13,23 @@ class BoardView(viewsets.ModelViewSet):
         serializer.save(creator=self.request.user)
 
     def get_queryset(self):
-        user = self.request.user
         querysetPublic = models.Board.objects.filter(visibility='pu')
-        querysetCreator = models.Board.objects.filter(creator=user)
-        queryset = querysetPublic.union(querysetCreator)
+        querysetCreator = models.Board.objects.filter(creator=self.request.user)
+        queryset = querysetPublic | querysetCreator
 
         return queryset
+
+
+class ListView(viewsets.ModelViewSet):
+    lookup_field = 'id'
+    permission_classes = (IsAccessToList,)
+    queryset = models.List.objects.all()
+    serializer_class = serializers.ListSerializer
+
+    def perform_create(self, serializer):
+        ID = int(self.request.data['boardId'])
+        try:
+            board = models.Board.objects.get(id=ID)
+            serializer.save(board=board)
+        except models.Board.DoesNotExist:
+            raise RuntimeWarning("Board Object is None")
